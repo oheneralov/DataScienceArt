63878a08db0d3ffeaf94ae5a80e3e54cb56622f4
Oleksandr Generalov <oheneralov@gmail.com>
2021-08-02 11:12:49 +0300

    Update UserSeeder.js

--- CMS/website/database/seeds/UserSeeder.js
+++ CMS/website/database/seeds/UserSeeder.js
@@ -17,8 +17,8 @@ const Hash = use('Hash')
 class UserSeeder {
   async run () {
     //create an admin account
-    let passwd = await Hash.make('g5hsrhrh6r6r62050$@%^&')
-    let userPasswd = await Hash.make('rkfelhtge5g6r6srh5r5vg')
+    let passwd = await Hash.make('12345)
+    let userPasswd = await Hash.make('12345')
     // create a super-admin account
     await Database.table('users').insert({username: 'admin', password: passwd, is_admin: 1, email: 'admin@gmail.com', first_name: 'Oleksandr', last_name: 'Heneralov'})
   
